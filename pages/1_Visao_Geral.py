32b154bf4750142463c5c953e513ffc3d0c91d02
Gustavo Henrique <106562840+gustavohssBR@users.noreply.github.com>
2024-01-02 20:10:08 -0300

    Update 1_Visao_Geral.py

--- pages/1_Visao_Geral.py
+++ pages/1_Visao_Geral.py
@@ -153,8 +153,8 @@ def Limpeza_dados(df_):
     return df
 
 #COLETA DE DADOS
-#df_ = pd.read_csv(r'./dataset/zomato.csv')
-df_ = pd.read_csv(r'../dataset/zomato.csv')
+df_ = pd.read_csv(r'./dataset/zomato.csv')
+#df_ = pd.read_csv(r'../dataset/zomato.csv')
 
 df = Limpeza_dados(df_)
 
